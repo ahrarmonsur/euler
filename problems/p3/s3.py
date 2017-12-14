5411581856eb9f25da919cdd3508a9f37219385a
Ahrar Monsur <ahrarmonsur93@gmail.com>
2017-12-13 23:53:12 -0500

    Solves S3

---
+++ problems/p3/s3.py
@@ -0,0 +1,20 @@
+"""
+Problem 3
+Largest prime factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+def generate_prime_factors(n):
+    i = 2
+    while i**2 <= n:
+        if n % i:
+            i += 1
+        else:
+            yield i
+            n /= i
+    if n > 1:
+        yield n
+
+print max(generate_prime_factors(600851475143))
