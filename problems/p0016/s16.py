6c6a87c7f74b01805b593473b0aaa2b2bc9c78b5
Ahrar Monsur <ahrarmonsur93@gmail.com>
2017-12-28 17:17:29 -0500

    Solves P16

---
+++ problems/p0016/s16.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+Project Euler Problem 16
+Power digit sum
+Solved by Ahrar Monsur
+
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+"""
+
+print sum(map(int, str(2**1000)))
